d010b8297be5b4971f33c4e1e6ae6b40d8d6fb12
hoxovic <9694402+hoxovic@users.noreply.github.com>
2021-03-14 12:34:15 +0100

    Separate test input into separate types and variables

--- internal/encoder/encoder_test.go
+++ internal/encoder/encoder_test.go
@@ -1,55 +1,71 @@
 package encoder
 
 import (
-	"fmt"
 	"testing"
 )
 
+type alphabetLengthTest struct {
+	alphabet string
+	numBits  uint
+}
+
+var alphabetLengthTests = []alphabetLengthTest{
+	{"testÖ", 2},
+	{"0123456789bcdefghjkmnpqrstuvwxyz", 5},
+}
+
 func TestNewEncoder(t *testing.T) {
-	expected := map[string]uint{"testÖ": 2,
-		"0123456789bcdefghjkmnpqrstuvwxyz": 5}
-	for alphabet, nBits := range expected {
-		encoder := NewEncoder(alphabet)
-		if encoder.nBitsPerRune != nBits {
-			t.Errorf("Expected nBitsPerRune %d, actual nBitsPerRune %d", nBits, encoder.nBitsPerRune)
+	for _, expected := range alphabetLengthTests {
+		encoder := NewEncoder(expected.alphabet)
+		if encoder.nBitsPerRune != expected.numBits {
+			t.Errorf("Expected nBitsPerRune %d, actual nBitsPerRune %d", expected.numBits, encoder.nBitsPerRune)
 		}
 	}
+}
+
+type encodeDefaultAlphabetTest struct {
+	val     uint64
+	encoded []rune
+}
+
+var encodeDefaultAlphabetTests = []encodeDefaultAlphabetTest{
+	{4, []rune("000000000004")},
+	{128, []rune("000000000040")},
+	{^uint64(0), []rune("zzzzzzzzzzzz")},
 }
 
 func TestEncodeDefaultAlphabet(t *testing.T) {
-	expected := map[uint64][]rune{
-		4:          []rune("000000000004"),
-		128:        []rune("000000000040"),
-		^uint64(0): []rune("zzzzzzzzzzzz"),
-	}
-	for val, expected := range expected {
-		encoded := Base32encoder.encode(val)
+	for _, test := range encodeDefaultAlphabetTests {
+		encoded := Base32encoder.encode(test.val)
 		for i, codePoint := range encoded {
-			if codePoint != expected[i] {
-				fmt.Println(val)
-				t.Errorf("Expected %s, got %s", string(expected), string(encoded))
+			if codePoint != test.encoded[i] {
+				t.Errorf("Expected %s, got %s", string(test.encoded), string(encoded))
 			}
 		}
 	}
 }
 
+type customAlphabetEncodingTest struct {
+	alphabet string
+	value    uint64
+	encoded  []rune
+}
+
+var customAlphabetEncodingTests = []customAlphabetEncodingTest{
+	{"0abc", 4, []rune("000000000000000000000000000000a0")},
+	{"0abc", ^uint64(0), []rune("cccccccccccccccccccccccccccccccc")},
+}
+
 func TestEncodeCustomAlphabet(t *testing.T) {
-	alphabet := `0abc`
-	encoder := NewEncoder(alphabet)
-	expected := map[uint64][]rune{
-		4:          []rune("000000000000000000000000000000a0"),
-		^uint64(0): []rune("cccccccccccccccccccccccccccccccc"),
-	}
-	for val, expected := range expected {
-		encoded := encoder.encode(val)
+	for _, test := range customAlphabetEncodingTests {
+		encoder := NewEncoder(test.alphabet)
+		encoded := encoder.encode(test.value)
 		for i, codePoint := range encoded {
-			if codePoint != expected[i] {
-				fmt.Println(val)
-				t.Errorf("Expected %s, got %s", string(expected), string(encoded))
+			if codePoint != test.encoded[i] {
+				t.Errorf("Expected %s, got %s", string(test.encoded), string(encoded))
 			}
 		}
 	}
-
 }
 
 func TestEncodeCustomAlphabetNotPowerOfTwoExpectTruncate(t *testing.T) {
